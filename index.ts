5330072b78a185151f036370429784d6f75eed69
yudofu502 <42887290+yudofu502@users.noreply.github.com>
2023-05-19 19:59:13 +0900

    helpコマンド追加

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import { APIEmbed, Client, Events, GatewayIntentBits } from 'discord.js'
-import { KeyvFile } from 'keyv-file'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
 
 import { Command } from './src/types/command'
 
@@ -8,6 +7,7 @@ import vc from './src/commands/vc.js'
 import team from './src/commands/team.js'
 import rank from './src/commands/rank.js'
 import call from './src/commands/call.js'
+import help from './src/commands/help'
 
 const http = require('http')
 http
@@ -25,7 +25,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
   ],
 })
-const commands = [map, vc, team, rank, call] as Command[]
+const commands = [map, vc, team, rank, call, help] as Command[]
 
 // Botが起動した時の処理
 client.once(Events.ClientReady, async () => {
@@ -44,34 +44,6 @@ client.once(Events.ClientReady, async () => {
 // コマンドが実行された時の処理
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return
-  if (interaction.commandName === 'help') {
-    await interaction.deferReply({ ephemeral: true })
-    const embed: APIEmbed = {
-      title: 'Botの使い方',
-      description: 'Valorantカスタム用のBotです',
-      fields: [
-        { name: '/help', value: 'このコマンドです\nBotの説明を表示します' },
-        { name: '/map', value: 'マップをランダムに選択します' },
-        {
-          name: '/vc',
-          value:
-            'VCの設定をします\nVC1,VC2,…は`/team`のチーム分けで使うVCでHomeは`/call`で集合先として使うVCです(指定無しの場合はVC1)',
-        },
-        { name: '/team', value: '同じVCにいるメンバーをランクを考慮してランダムにチーム分けします' },
-        {
-          name: '/call',
-          value: 'チーム分けしたメンバー全員を1つのVCに集合させます',
-        },
-        {
-          name: '/rank',
-          value:
-            '自分のランクを設定または確認します\nランクは`/team`でチーム分けする際に考慮されます\nランクは`ゴールド2`か`G2`のように指定してください（',
-        },
-      ],
-      footer: { text: 'made by secchanu' },
-    }
-    await interaction.followUp({ embeds: [embed] })
-  }
 
   const command = commands.find((cmd) => cmd.name === interaction.commandName)
 
---
+++ src/commands/help.ts
@@ -0,0 +1,35 @@
+import { APIEmbed } from 'discord.js'
+import { Command } from '../types/command'
+
+export default {
+  commandType: 'global',
+  name: 'help',
+  description: 'Botの説明を表示します',
+  async execute(interaction) {
+    const embed: APIEmbed = {
+      title: 'Botの使い方',
+      description: 'Valorantカスタム用のBotです',
+      fields: [
+        { name: '/help', value: 'このコマンドです\nBotの説明を表示します' },
+        { name: '/map', value: 'マップをランダムに選択します' },
+        {
+          name: '/vc',
+          value:
+            'VCの設定をします\nVC1,VC2,…は`/team`のチーム分けで使うVCでHomeは`/call`で集合先として使うVCです(指定無しの場合はVC1)',
+        },
+        { name: '/team', value: '同じVCにいるメンバーをランクを考慮してランダムにチーム分けします' },
+        {
+          name: '/call',
+          value: 'チーム分けしたメンバー全員を1つのVCに集合させます',
+        },
+        {
+          name: '/rank',
+          value:
+            '自分のランクを設定または確認します\nランクは`/team`でチーム分けする際に考慮されます\nランクは`ゴールド2`か`G2`のように指定してください（',
+        },
+      ],
+      footer: { text: 'made by secchanu' },
+    }
+    await interaction.followUp({ embeds: [embed] })
+  },
+} as Command
