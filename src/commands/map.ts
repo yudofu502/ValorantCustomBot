cebcb177b2450fbb25dcae243c9eb67413795774
yudofu502 <42887290+yudofu502@users.noreply.github.com>
2023-05-19 20:13:16 +0900

    mapコマンドリファクター

--- src/commands/map.ts
+++ src/commands/map.ts
@@ -2,7 +2,7 @@ import { Command } from '../types/command'
 import Keyv from 'keyv'
 import fetch from 'isomorphic-fetch'
 
-type Map = {
+type ValoMap = {
   id: string
   name: string
   img: string
@@ -36,23 +36,26 @@ export default {
 const mapsCache = new Keyv({
   namespace: 'maps',
 })
+const ignoreIds = ['ee613ee9-28b7-4beb-9666-08db13bb2244'] // 射撃場は除外
 
-const getMap = async (): Promise<Map | undefined> => {
+const getMap = async (): Promise<ValoMap | undefined> => {
   const cached = await mapsCache.get('maps')
 
-  let mapsData: Map[] = [];
+  let mapsData: ValoMap[] = []
 
   if (!cached) {
     const mapApi = `https://valorant-api.com/v1/maps?language=ja-JP`
     const res = await fetch(mapApi)
     const data = await res.json()
-    mapsData = data.data.map((map: any) => {
-      return { id: map.uuid, name: map.displayName, img: map.splash } as Map
-    })
+    mapsData = data.data
+      .filter((map: any) => !ignoreIds.includes(map.uuid))
+      .map((map: any) => {
+        return { id: map.uuid, name: map.displayName, img: map.splash } as ValoMap
+      })
 
     mapsCache.set('maps', mapsData, 60 * 60 * 1000) //ソースからの更新頻度
   }
-  const maps: Map[] = cached ?? mapsData ?? []
+  const maps: ValoMap[] = cached ?? mapsData ?? []
   const map = maps[Math.floor(Math.random() * maps.length)]
   return map
 }
