496b398f925cdee1537edb250c6bb0d8fced7b24
yudofu502 <42887290+yudofu502@users.noreply.github.com>
2023-05-23 12:23:53 +0000

    修正

--- src/commands/match.ts
+++ src/commands/match.ts
@@ -60,12 +60,11 @@ export default {
         setRatio(userId, newRating)
         const newRank = ratioToRank(newRating)
         if (rank !== newRank) {
-          message += `\n<@${userId}>さんのランクが${rank?.emoji ?? ''}${rank.fullName}から${newRank?.emoji ?? ''}${
-            newRank.fullName
-          }に変動しました`
+          message += `\n<@${userId}>さんのランクが${rank?.emoji ?? ''}${rank.fullName}から${newRank?.emoji ?? ''}${newRank.fullName
+            }に変動しました`
         }
       }
     }
-    await interaction.reply(message)
+    await interaction.followUp(message)
   },
 } as Command
