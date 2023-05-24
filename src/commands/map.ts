8767a2faaa8200d0a8c4e62636b5c723433b787e
yudofu502 <42887290+yudofu502@users.noreply.github.com>
2023-05-24 20:44:50 +0900

    mapsオプションにignoreを追加

--- src/commands/map.ts
+++ src/commands/map.ts
@@ -1,6 +1,7 @@
 import { Command } from '../types/command'
 import Keyv from 'keyv'
 import fetch from 'isomorphic-fetch'
+import { ApplicationCommandOptionType } from 'discord.js'
 
 type ValoMap = {
   id: string
@@ -12,25 +13,32 @@ export default {
   commandType: 'global',
   name: 'map',
   description: 'ランダムにマップを選択します',
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: 'ignore',
+      description: '除外するマップを設定（,区切り）',
+    },
+  ],
   async execute(interaction) {
-    const map = await getMap()
+    const ignore = interaction.options.getString('ignore')?.split(',')
+    const map = await getMap(ignore)
     if (!map) {
       await interaction.reply('マップが見つかりませんでした')
       return
     }
 
-    await interaction.reply(
-      {
-        content: 'マップを選択しました',
-        embeds: [
-          {
-            title: map.name,
-            image: {
-              url: map.img,
-            },
+    await interaction.reply({
+      content: 'マップを選択しました',
+      embeds: [
+        {
+          title: map.name,
+          image: {
+            url: map.img,
           },
-        ],
-      })
+        },
+      ],
+    })
   },
 } as Command
 
@@ -39,7 +47,7 @@ const mapsCache = new Keyv({
 })
 const ignoreIds = ['ee613ee9-28b7-4beb-9666-08db13bb2244'] // 射撃場は除外
 
-const getMap = async (): Promise<ValoMap | undefined> => {
+const getMap = async (ignore?: String[]): Promise<ValoMap | undefined> => {
   const cached = await mapsCache.get('maps')
 
   let mapsData: ValoMap[] = []
@@ -56,7 +64,7 @@ const getMap = async (): Promise<ValoMap | undefined> => {
 
     mapsCache.set('maps', mapsData, 60 * 60 * 1000) //ソースからの更新頻度
   }
-  const maps: ValoMap[] = cached ?? mapsData ?? []
+  const maps: ValoMap[] = (cached ?? mapsData ?? []).filter((map: ValoMap) => !ignore || !ignore.includes(map.name))
   const map = maps[Math.floor(Math.random() * maps.length)]
   return map
 }
